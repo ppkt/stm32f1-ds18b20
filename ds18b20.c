30dbb9e4873e9e594c293932a18037a0e6ee31a5
ppkt <github@ppkt.eu>
2015-03-12 18:41:24 +0100

    refactoring of library

--- ds18b20.c
+++ ds18b20.c
@@ -1,16 +1,23 @@
 #include "ds18b20.h"
 
 u8 precission = 3;
-static one_wire_device *one_wire_list_of_devices = 0;
-u8 one_wire_devices_list_size;
+one_wire_device *one_wire_list_of_devices;
+u8 one_wire_devices_list_size = 2;
 
 void ds18b20_init(GPIO_TypeDef *gpio, u16 port, TIM_TypeDef *timer) {
 	one_wire_init(gpio, port, timer);
 	one_wire_list_of_devices = one_wire_search_rom(&one_wire_devices_list_size);
 }
 
-u8 ds18b20_get_precission() {
+u8 ds18b20_get_precission(void) {
 	return precission;
+}
+
+ds18b20_devices ds18b20_get_devices() {
+    ds18b20_devices ret;
+    ret.size = one_wire_devices_list_size;
+    ret.devices = one_wire_list_of_devices;
+    return ret;
 }
 
 void ds18b20_set_precission(u8 p) {
@@ -26,7 +33,7 @@ void ds18b20_set_precission(u8 p) {
 	one_wire_write_byte(0x1F | (precission << 5));
 }
 
-void ds18b20_convert_temperature_simple() {
+void ds18b20_convert_temperature_simple(void) {
 	one_wire_reset_pulse();
 	one_wire_write_byte(0xCC); // Skip ROM
 	one_wire_write_byte(0x44); // Convert temperature
@@ -38,40 +45,45 @@ void ds18b20_convert_temperature(one_wire_device device) {
 	one_wire_write_byte(0x44); // Convert temperature
 }
 
-void ds18b20_convert_temperature_all() {
+void ds18b20_convert_temperature_all(void) {
 	u8 i = 0;
 	for (i = 0; i < one_wire_devices_list_size; ++i) {
 		ds18b20_convert_temperature(one_wire_list_of_devices[i]);
 	}
 }
 
-void ds18b20_read_temperature_simple() {
+simple_float ds18b20_read_temperature_simple(void) {
 	one_wire_reset_pulse();
 	one_wire_write_byte(0xCC); // Skip ROM
 	one_wire_write_byte(0xBE); // Read scratchpad
 
-	ds18b20_decode_temperature();
+	return ds18b20_decode_temperature();
 }
 
-void ds18b20_read_temperature(one_wire_device device) {
+simple_float ds18b20_read_temperature(one_wire_device device) {
 	one_wire_reset_pulse();
 	one_wire_match_rom(device); // Match ROM
 	one_wire_write_byte(0xBE); // Read scratchpad
 
-	ds18b20_decode_temperature();
+	return ds18b20_decode_temperature();
 }
 
-void ds18b20_read_temperature_all() {
+simple_float* ds18b20_read_temperature_all(void) {
 	u8 i = 0;
+	simple_float *temperatures;
+	temperatures = malloc(one_wire_devices_list_size * sizeof(simple_float));
+
 	for (i = 0; i < one_wire_devices_list_size; ++i) {
-		ds18b20_read_temperature(one_wire_list_of_devices[i]);
+		temperatures[i] = ds18b20_read_temperature(one_wire_list_of_devices[i]);
 	}
+	return temperatures;
 }
 
-void ds18b20_decode_temperature() {
+simple_float ds18b20_decode_temperature(void) {
 	int i;
 	u8 crc;
 	u8 data[9];
+    simple_float f;
 	one_wire_reset_crc();
 
 	for (i = 0; i < 9; ++i) {
@@ -79,17 +91,24 @@ void ds18b20_decode_temperature() {
 		crc = one_wire_crc(data[i]);
 	}
 	if (crc != 0) {
-		return;
+		return f;
 	}
 
 	u8 temp_msb = data[1]; // Sign byte + lsbit
 	u8 temp_lsb = data[0]; // Temp data plus lsb
     float temp = (temp_msb << 8 | temp_lsb) / powf(2, 4);
     int rest = (temp - (int)temp) * 1000.0;
-    printf("%d.%d\r", (int)temp, rest);
+
+    f.integer = (u8)temp;
+    f.fractional = rest;
+
+    char buffer[10];
+    sprintf(buffer, "%d.%d\r", (int)temp, rest);
+    usart2_print(buffer);
+    return f;
 }
 
-void ds18b20_wait_for_conversion() {
+void ds18b20_wait_for_conversion(void) {
 	if (precission == 0) {
 		delay(95);
 	} else if (precission == 1) {
--- ds18b20.h
+++ ds18b20.h
@@ -3,11 +3,32 @@
 #include "one_wire.h"
 #include "math.h"
 #include "utils.h"
+#include "usart.h"
+#include <stdlib.h>
+
+// Structure in which temperature is stored
+typedef struct {
+    s8 integer;
+    u16 fractional;
+} simple_float;
+
+// Structure for returning list of devices on one wire
+typedef struct {
+    u8 size;
+    one_wire_device *devices;
+} ds18b20_devices;
 
 void ds18b20_init(GPIO_TypeDef *gpio, u16 port, TIM_TypeDef *timer);
 void ds18b20_set_precission(u8 precission);
-void ds18b20_convert_temperature_simple();
-void ds18b20_read_temperature_simple();
-void ds18b20_wait_for_conversion();
+ds18b20_devices ds18b20_get_devices();
+
+void ds18b20_convert_temperature_simple(void);
+simple_float ds18b20_read_temperature_simple(void);
+
+void ds18b20_convert_temperature_all(void);
+simple_float* ds18b20_read_temperature_all(void);
+
+void ds18b20_wait_for_conversion(void);
+simple_float ds18b20_decode_temperature(void);
 
 #endif // __DS18B20_H__
